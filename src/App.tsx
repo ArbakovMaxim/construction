2a3a187c294446d2a38b76988967d8c3c76f7436
Max Arbakov <fefaodessa@gmail.com>
2023-08-14 15:53:04 +0300

    start pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { ToastContainer } from "react-toastify";
+import { Route, Routes } from "react-router-dom";
+import { lazy, Suspense } from "react";
 
-function App() {
+const Layout = lazy(() => import("./layout/Layout"));
+const Home = lazy(() => import("./pages/Home"));
+const PageNotFound = lazy(() => import("./pages/PageNotFound/PageNotFound"));
+
+export const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Suspense fallback={<h1>Loading....</h1>}>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />}></Route>
+          </Route>
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </Suspense>
+      <ToastContainer />
+    </>
   );
-}
-
-export default App;
+};
---
+++ src/layout/Layout.tsx
@@ -0,0 +1,18 @@
+import { Suspense } from "react";
+import { Outlet } from "react-router-dom";
+import { Header } from "../components/header/Header";
+import { Footer } from "../components/footer/Footer";
+
+const Layout = () => {
+  return (
+    <>
+      <Suspense fallback={null}>
+        <Header />
+        <Outlet />
+        <Footer />
+      </Suspense>
+    </>
+  );
+};
+
+export default Layout;
