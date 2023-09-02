9affd7f9e8198f057535faaa86e7a605f0bf0054
Max Arbakov <fefaodessa@gmail.com>
2023-09-02 23:36:43 +0300

    add News page

--- src/components/newsCategories/NewsCategories.tsx
+++ src/components/newsCategories/NewsCategories.tsx
@@ -1,28 +1,79 @@
 import "./NewsCategories.css";
 import "../../ui/container.css";
+import { useState } from "react";
 
 export const NewsCategories = () => {
+  const [activ, setActiv] = useState("all");
+
   return (
     <section className="newsCategories__section">
       <div className="container">
-        <h2>Categories</h2>
+        <h2 className="newsCategories__title">Categories</h2>
         <ul className="newsCategories__list">
-          <li className="newsCategories__item" onClick={() => {}}>
+          <li
+            className={
+              "newsCategories__item " +
+              (activ === "all" ? "newsCategories__item--activ" : "")
+            }
+            onClick={() => {
+              setActiv("all");
+            }}
+          >
             All News
           </li>
-          <li className="newsCategories__item" onClick={() => {}}>
+          <li
+            className={
+              "newsCategories__item " +
+              (activ === "company" ? "newsCategories__item--activ" : "")
+            }
+            onClick={() => {
+              setActiv("company");
+            }}
+          >
             Company News
           </li>
-          <li className="newsCategories__item" onClick={() => {}}>
+          <li
+            className={
+              "newsCategories__item " +
+              (activ === "innovation" ? "newsCategories__item--activ" : "")
+            }
+            onClick={() => {
+              setActiv("innovation");
+            }}
+          >
             Innovation
           </li>
-          <li className="newsCategories__item" onClick={() => {}}>
+          <li
+            className={
+              "newsCategories__item " +
+              (activ === "industry" ? "newsCategories__item--activ" : "")
+            }
+            onClick={() => {
+              setActiv("industry");
+            }}
+          >
             Industry News
           </li>
-          <li className="newsCategories__item" onClick={() => {}}>
+          <li
+            className={
+              "newsCategories__item " +
+              (activ === "expert" ? "newsCategories__item--activ" : "")
+            }
+            onClick={() => {
+              setActiv("expert");
+            }}
+          >
             Expert Tips
           </li>
-          <li className="newsCategories__item" onClick={() => {}}>
+          <li
+            className={
+              "newsCategories__item " +
+              (activ === "marketing" ? "newsCategories__item--activ" : "")
+            }
+            onClick={() => {
+              setActiv("marketing");
+            }}
+          >
             Marketing
           </li>
         </ul>
